a213e583b9b8c9c771c64c41c89e6be3a6fbbdb9
Dante Tucker <dante@dantetucker.com>
2017-10-02 02:13:48 -0400

    Fix walking animation

--- FirstPersonControls.js
+++ FirstPersonControls.js
@@ -147,19 +147,19 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 		switch ( event.keyCode ) {
 
 			case 38: /*up*/
-			case 87: /*W*/ this.moveForward = true; p1.setstate.moving = true; break;
+			case 87: /*W*/ this.moveForward = true; break;
 
 			case 37: /*left*/
-			case 65: /*A*/ this.moveLeft = true; p1.setstate.moving = true; break;
+			case 65: /*A*/ this.moveLeft = true; break;
 
 			case 40: /*down*/
-			case 83: /*S*/ this.moveBackward = true; p1.setstate.moving = true; break;
+			case 83: /*S*/ this.moveBackward = true; break;
 
 			case 39: /*right*/
-			case 68: /*D*/ this.moveRight = true; p1.setstate.moving = true; break;
+			case 68: /*D*/ this.moveRight = true; break;
 
-			case 82: /*R*/ this.moveUp = true; p1.setstate.moving = true; break;
-			case 70: /*F*/ this.moveDown = true; p1.setstate.moving = true; break;
+			case 82: /*R*/ this.moveUp = true; break;
+			case 70: /*F*/ this.moveDown = true; break;
 			
 			case 69: /*action*/ actions.check(); break;
 			case 84: /*chatshow*/ chatsystem.toggleChatbar(true); break;
@@ -173,19 +173,19 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 		switch ( event.keyCode ) {
 
 			case 38: /*up*/
-			case 87: /*W*/ this.moveForward = false; p1.setstate.moving = false; break;
+			case 87: /*W*/ this.moveForward = false; break;
 
 			case 37: /*left*/
-			case 65: /*A*/ this.moveLeft = false; p1.setstate.moving = false; break;
+			case 65: /*A*/ this.moveLeft = false; break;
 
 			case 40: /*down*/
-			case 83: /*S*/ this.moveBackward = false; p1.setstate.moving = false; break;
+			case 83: /*S*/ this.moveBackward = false; break;
 
 			case 39: /*right*/
-			case 68: /*D*/ this.moveRight = false; p1.setstate.moving = false; break;
+			case 68: /*D*/ this.moveRight = false; break;
 
-			case 82: /*R*/ this.moveUp = false; p1.setstate.moving = false; break;
-			case 70: /*F*/ this.moveDown = false; p1.setstate.moving = false; break;
+			case 82: /*R*/ this.moveUp = false; break;
+			case 70: /*F*/ this.moveDown = false; break;
 
 		}
 
@@ -218,6 +218,12 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 
 		if ( this.moveUp ) this.object.translateY( actualMoveSpeed );
 		if ( this.moveDown ) this.object.translateY( - actualMoveSpeed );
+
+		if ( this.moveForward || this.moveBackward || this.moveLeft || this.moveRight ){
+			p1.setstate.moving = true;
+		} else {
+			p1.setstate.moving = false;
+		}
 
 		var actualLookSpeed = delta * this.lookSpeed;
 
